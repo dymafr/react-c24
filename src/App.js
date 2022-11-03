fe80840cf97cdeb577542380c9260b489b9dd2d3
dymafr <contact@dyma.fr>
2022-11-03 10:29:26 +0100

    l2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { useRecoilValue } from 'recoil';
+import AddTodo from './components/AddTodo';
+import { todosState } from './recoil';
+
 function App() {
+  const todos = useRecoilValue(todosState);
+  console.log(todos);
+
   return (
     <div className="d-flex flex-row justify-content-center align-items-center p-20">
       <div className="card container p-20">
         <h1 className="mb-20">Todo list</h1>
+        <AddTodo />
       </div>
     </div>
   );
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,5 +1,41 @@
+import { useState } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { todosState } from '../recoil';
+
 function AddTodo() {
-  return <h2>AddTodo</h2>;
+  const [inputValue, setInputValue] = useState('');
+  const setTodosState = useSetRecoilState(todosState);
+
+  function handleOnChange(e) {
+    setInputValue(e.target.value);
+  }
+
+  async function handleClick() {
+    setTodosState((oldTodosState) => [
+      ...oldTodosState,
+      {
+        _id: crypto.randomUUID(),
+        content: inputValue,
+        done: false,
+        edit: false,
+      },
+    ]);
+    setInputValue('');
+  }
+
+  return (
+    <div className="d-flex align-items-center mb-20">
+      <input
+        value={inputValue}
+        onChange={handleOnChange}
+        className="flex-fill mr-15"
+        type="text"
+      />
+      <button onClick={handleClick} className="btn btn-primary">
+        Ajouter todo
+      </button>
+    </div>
+  );
 }
 
 export default AddTodo;
